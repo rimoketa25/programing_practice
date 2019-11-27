632e74a6ea2cdbff6b7a56b8ec1f834f9e8ac087
rimoketa <t.takemori25@gmail.com>
2019-11-27 11:04:46 +0900

    フォルダ構成の変更

--- HTML,CSS/MyCSSVariables/js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-{
-  const btn = document.getElementById('btn');
-  btn.addEventListener('click', () => {
-    document.documentElement.style.setProperty('--my-hue', Math.floor(Math.random() * 360));
-  });
-}
